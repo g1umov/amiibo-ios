411024858b81de27fe6b38dd65ab406508f18263
g1umov <vladislavglumov30@gmail.com>
2024-07-12 17:50:37 +0200

    Create Amiibo Release data model

---
+++ amiibo-ios/AmiiboRelease.h
@@ -0,0 +1,12 @@
+//
+//  AmiiboRelease.h
+//  amiibo-ios
+//
+//  Created by Vladislav Glumov on 12.7.24..
+//
+
+#ifndef AmiiboRelease_h
+#define AmiiboRelease_h
+
+
+#endif /* AmiiboRelease_h */
---
+++ amiibo-ios/AmiiboRelease.m
@@ -0,0 +1,8 @@
+//
+//  AmiiboRelease.m
+//  amiibo-ios
+//
+//  Created by Vladislav Glumov on 12.7.24..
+//
+
+#import <Foundation/Foundation.h>
